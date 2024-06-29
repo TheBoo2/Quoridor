a4495c73cc90732b05e05728627d46420f0c2152
MekaShak <shakbored@gmail.com>
2024-06-29 16:04:00 -0500

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -3,6 +3,7 @@ package Quoridor;
 public class Juego {
     public static void main(String[] args) {
 JFrame cuadro = new JFrame();
+cuadro.setLayout(new GridBagLayout());
 cuadro.setMinimumSize(new Dimension(500, 500);
 cuadro.setLocationRelativeTo(null);
 
