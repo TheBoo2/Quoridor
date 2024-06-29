0ef9e8172ada356254702361c6e20d9964689392
MekaShak <shakbored@gmail.com>
2024-06-29 14:32:46 -0500

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,5 +1,15 @@
 package Quoridor;
+import javax.swing.*;
+import java.awt.*;
 
-public class Tablero {
+
+public class Tablero extends JPanel{
+public int TileSize = 90;
+int colum = 17;
+int fil = 17;
+
+public Tablero(){
+this.setPreferredSize(new Dimension(colum * TileSize, fil * TileSize));
+}
         
 }
