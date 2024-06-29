40bdab4d154e4155a39eaccba961f7a41af60db8
MekaShak <shakbored@gmail.com>
2024-06-29 14:24:07 -0500

    Update main con JFrame

--- Juego.java
+++ Juego.java
@@ -2,7 +2,10 @@ package Quoridor;
 
 public class Juego {
     public static void main(String[] args) {
-
+JFrame cuadro = new JFrame();
+frame.setMinimumSize(new Dimension(500, 500);
+frame.setLocationRelativeTo(null);
+frame.setVisible(true);
 
     }
 }
