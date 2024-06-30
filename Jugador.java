44444ad339f70c890b108c829bf764a676e044fb
MekaShak <shakbored@gmail.com>
2024-06-30 14:12:36 -0500

    Agregué funciones que simplifican el movimiento con row y col

--- Jugador.java
+++ Jugador.java
@@ -1,5 +1,43 @@
 package Quoridor;
 
 public class Jugador {
-    
+        private String name;
+    private int wallsLeft;
+    private int row; 
+    private int col; 
+
+    public Player(String name, int initialWalls) {
+        this.name = name;
+        this.wallsLeft = initialWalls;
+        
+        this.row = 0;
+        this.col = 4 ;   }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWallsLeft() {
+        return wallsLeft;
+    }
+
+    public void decrementWallsLeft() {
+        wallsLeft--;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
